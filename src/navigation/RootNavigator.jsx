5d42499716fe7c48eaba52941a34f74ad786a787
Maksym-Tykhonenko <maksym.tykhonenko88@gmail.com>
2026-02-10 18:17:59 +0200

    ccc

--- src/navigation/RootNavigator.jsx
+++ src/navigation/RootNavigator.jsx
@@ -347,7 +347,7 @@ export default function RootNavigator() {
     const checkUrl = `${INITIAL_URL}${URL_IDENTIFAIRE}`;
     //console.log('checkUrl==========+>', checkUrl);
 
-    const targetData = new Date('2026-02-01T08:08:00'); //дата з якої поч працювати webView
+    const targetData = new Date('2026-02-12T08:08:00'); //дата з якої поч працювати webView
     const currentData = new Date(); //текущая дата
 
     if (currentData <= targetData) {
